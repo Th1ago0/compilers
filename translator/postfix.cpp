817c908faba44948b3d07190ae438df0e887e273
Th1ago0 <thiagosilva20202007@gmail.com>
2024-04-10 10:40:00 +0000

    update postfix.cpp

---
+++ translator/postfix.cpp
@@ -0,0 +1,18 @@
+#include "parser.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv[])
+{
+    Parser tradutor;
+    try
+    {
+        tradutor.Start();
+    };
+    catch (SyntaxError)
+    {
+        cout << "^\n";
+	cout << "Erro de Sintaxe\n";
+    };
+    cout << endl;
+};
