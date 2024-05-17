eca72347e2a2d9aa1a6e794617e5b17b8570efc2
Th1ago0 <thiagosilva20202007@gmail.com>
2024-05-17 21:23:27 +0000

    update lexer

--- python_translator/lexer.py
+++ python_translator/lexer.py
@@ -1,5 +1,3 @@
-import pdb
-
 class Tag:
     def __init__(self):
         self.ID = "ID"
@@ -7,139 +5,115 @@ class Tag:
         self.TRUE = "TRUE"
         self.FALSE = "FALSE"
         self.EOF = "EOF"
-
+        
 class Token:
     def __init__(self, tag):
         self.tag = tag
-
-    def to_string(self):
+        
+    def str(self):
         return self.tag
-
+        
 class Id(Token):
     def __init__(self, tag, name):
         self.tag = tag
         self.name = name
         super().__init__(self.tag)
-
-    def to_string(self):
+        
+    def str(self):
         return self.name
-
+        
 class Num(Token):
     def __init__(self, value):
         self.value = value
         self.tag = Tag().NUM
         super().__init__(self.tag)
-
-    def to_string(self):
+        
+    def str(self):
         return str(self.value)
-
+        
 class TokenHandler:
     def __init__(self):
         self.t = None
         self.n = None
         self.i = None
         
-
 class Lexer:
-    def __init__(self, source):
-
-        self.source_code = source
-        self.index = 0
-
+    def __init__(self, file_name):
+        self.file = open(file_name, "r")
         self.line = 1
-        self.peek = self.source_code[self.index]
-
+        self.peek = self.file.read(1)
+        
         self.id_table = {}
         self.id_table["false"] = Id(Tag().FALSE, "false")
         self.id_table["true"] = Id(Tag().TRUE, "true")
-        
         self.token = TokenHandler()
-
+        
     def next_char(self):
-        if len(self.source_code) > (self.index + 1):
-            self.index += 1
-            self.peek = self.source_code[self.index]
-        else:
-            self.peek = " "
-    
+        self.peek = self.file.read(1)
+        
     def is_space(self):
         if self.peek == "\n":
             self.line += 1
         self.next_char()
-
+        
     def is_digit(self):
         v = 0
-        n = int(self.peek)
-        v = 10 * v + n
-        self.next_char()
-
         while self.peek.isdigit():
-            n = int(self.peek)
-            v = 10 * v + n
+            v = 10 * v + int(self.peek)
             self.next_char()
-
+            
         self.token.n = Num(v)
         return self.token.n
-
+        
     def is_alpha(self):
-        s = self.peek
-        self.next_char()
-
+        s = ''
         while self.peek.isalpha():
             s += self.peek
             self.next_char()
-
+            
         value = self.id_table.get(s)
-
         if value is not None:
             self.token.i = value
             return self.token.i
-
         new_id = Id(Tag().ID, s)
         self.id_table[s] = new_id
-
+        
         self.token.i = new_id
         return self.token.i
-    
+        
+    def skip_comments(self):
+        self.next_char()
+        if self.peek == "/":
+            while self.peek != "\n" and self.peek != "":
+                self.next_char()
+            self.next_char()
+            return self.scan()
+        else:
+            self.token.t = Token("/")
+            return self.token.t
+        
     def lineno(self):
         return self.line
-
-    def scan(self):
-        #pdb.set_trace()
-        if self.index == len(self.source_code):
-            eof = Token(Tag().EOF)
-            return eof
         
+    def scan(self):
         while self.peek.isspace():
             self.is_space()
-
+            
         if self.peek.isdigit():
             return self.is_digit()
-        
-        if self.peek.isalpha():
+            
+        elif self.peek.isalpha():
             return self.is_alpha()
-
-        if self.peek == "/":
-            if self.source_code[self.index + 1] == "/":
-                self.next_char()
-                self.next_char()
-                while self.peek != "\n":
-                    self.next_char()
-                self.next_char()
+            
+        elif self.peek == "/":
+            return self.skip_comments()
                 
-            elif self.source_code[self.index + 1] == "*":
-                self.next_char()
-                self.next_char()
-                while self.peek != "*" and self.source_code[self.index + 1] != "/":
-                    self.next_char()
-                self.next_char()
-                self.next_char()
+        elif self.peek == '':
+            return Token(Tag().EOF)
         
-        if not (self.peek.isalnum() or self.peek.isspace()):
+        else:
             symbol = Token(self.peek)
             peek = ' '
             self.next_char()
             self.token.t = symbol
             return self.token.t
-        else:
-            self.scan()
